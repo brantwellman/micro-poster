fd23d6d395ef2692f2787c0b0de5284edf7edbe9
Brant Wellman <BrantWellman@gmail.com>
2016-12-01 19:50:48 -0700

    Add flash messages partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to Macro-Poster!"
+      flash[:success] = "Welcome to Macro-Poster, #{@user.name}!"
       redirect_to @user
     else
       render 'new'
