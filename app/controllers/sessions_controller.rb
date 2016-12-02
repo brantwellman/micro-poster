03edcf26652062e564d9d4be4697d052551b33ef
Brant Wellman <BrantWellman@gmail.com>
2016-12-02 12:20:09 -0700

    Add invalid login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,19 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # flash message - welcome user
+      # redirect user_path(user)
+    else
+      # flash error message
+      flash.now[:danger] = 'Sorry that email or password is not correct'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
