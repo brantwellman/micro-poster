6b2572d7b5309856ae48b340996b276133b3aae6
Brant Wellman <BrantWellman@gmail.com>
2016-12-12 10:24:40 -0700

    Add Send email in User create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in(@user)
-      flash[:success] = "Welcome to Macro-Poster, #{@user.name}!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_path
+      # log_in(@user)
+      # flash[:success] = "Welcome to Macro-Poster, #{@user.name}!"
+      # redirect_to @user
     else
       render 'new'
     end
