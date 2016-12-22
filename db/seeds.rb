e0d1c7f47732c80c8c0c6827056d287e16120914
Brant Wellman <BrantWellman@gmail.com>
2016-12-22 16:14:36 -0700

    Add styling for user.microposts, microposts seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-p User.create!(name:  "Brant",
+p User.create!(name:  "Brant User",
              email: "brantwellman@gmail.com",
              password:              "password",
              password_confirmation: "password",
@@ -8,6 +8,34 @@ p User.create!(name:  "Brant",
 
 p User.create!(name:  "Example User",
              email: "example@email.com",
+             password:              "password",
+             password_confirmation: "password",
+             activated: true,
+             activated_at: Time.zone.now)
+
+p User.create!(name:  "Dan User",
+            email: "dan@email.com",
+            password:              "password",
+            password_confirmation: "password",
+            activated: true,
+            activated_at: Time.zone.now)
+
+p User.create!(name:  "Other User",
+             email: "other@email.com",
+             password:              "password",
+             password_confirmation: "password",
+             activated: true,
+             activated_at: Time.zone.now)
+
+p User.create!(name:  "Loser User",
+            email: "loser@email.com",
+            password:              "password",
+            password_confirmation: "password",
+            activated: true,
+            activated_at: Time.zone.now)
+
+p User.create!(name:  "Winner User",
+             email: "winner@email.com",
              password:              "password",
              password_confirmation: "password",
              activated: true,
@@ -24,4 +52,10 @@ p User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
   p user
+end
+
+users = User.order(:created_at).take(6)
+50.times do |n|
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
