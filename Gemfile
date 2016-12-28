fbafa216176dd84c4574b480015193507929da55
Brant Wellman <BrantWellman@gmail.com>
2016-12-28 11:25:29 -0700

    Add picture upload functionality and tests

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'will_paginate', '~>3.1.0'
 gem 'will_paginate-materialize', github: 'harrybournis/will_paginate-materialize'
 gem 'responders'
 gem 'active_model_serializers', '~> 0.10.0'
+gem 'carrierwave', '0.11.2'
+gem 'mini_magick', '4.5.1'
+gem 'fog', '1.38.0'
 
 group :development, :test do
   gem 'byebug'
--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -10,6 +10,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'ul.pagination', count: 1
+    assert_select 'input[type=file]'
     # Invalid mp submission
     assert_no_difference "Micropost.count" do
       post microposts_path, params: { micropost: { content: "" } }
@@ -17,10 +18,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     # Valid mp submission
     content = "This is some test content for the post"
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference "Micropost.count", 1 do
-      post microposts_path, params: { micropost: { content: content } }
+      post microposts_path, params: { micropost: { content: content, picture: picture } }
     end
-    assert_redirected_to root_path
+    assert Micropost.first.picture?
     follow_redirect!
     assert_match content, response.body
     assert_select 'span.content'
