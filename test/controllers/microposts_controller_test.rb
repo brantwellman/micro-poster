c9f3f70309053386d8287436edf81589c5741868
Brant Wellman <BrantWellman@gmail.com>
2016-12-28 10:32:52 -0700

    Add MP controller and integration tests

--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -19,4 +19,13 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to login_path
   end
+
+  test "should not delete mps and redirect when not proper user" do
+    log_in_as(users(:brant))
+    micropost = microposts(:ants)
+    assert_no_difference "Micropost.count" do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_path
+  end
 end
---
+++ test/integration/microposts_interface_test.rb
@@ -0,0 +1,51 @@
+require 'test_helper'
+
+class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:brant)
+  end
+
+  test "microposts interface" do
+    log_in_as(@user)
+    get root_path
+    assert_select 'ul.pagination', count: 1
+    # Invalid mp submission
+    assert_no_difference "Micropost.count" do
+      post microposts_path, params: { micropost: { content: "" } }
+    end
+    assert_select 'div#error_explanation'
+    # Valid mp submission
+    content = "This is some test content for the post"
+    assert_difference "Micropost.count", 1 do
+      post microposts_path, params: { micropost: { content: content } }
+    end
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_match content, response.body
+    assert_select 'span.content'
+    # Delete mp
+    assert_select 'a', text: 'Delete'
+    first_micropost = @user.microposts.paginate(page: 1).first
+    assert_difference "Micropost.count", -1 do
+      delete micropost_path(first_micropost)
+    end
+    assert_redirected_to root_path
+    # Visit different user - no delete links
+    get user_path(users(:dan))
+    assert_select 'a', text: 'Delete', count: 0
+  end
+
+  test "micropost sidebar count" do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    user_2 = users(:doubleA)
+    log_in_as(user_2)
+    get root_path
+    assert_match "0 microposts", response.body
+    user_2.microposts.create!(content: "new micropost")
+    get root_path
+    assert_match "1 micropost", response.body
+  end
+end
